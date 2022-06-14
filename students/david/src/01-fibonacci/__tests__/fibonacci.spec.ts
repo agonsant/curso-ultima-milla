4230073c1e688156dd0d994857b6d864d5d4df80
David Galvez <davidgalvezgomez01@gmail.com>
2022-06-14 13:10:56 +0200

    feat: added test for iterativeFibonacci function

--- students/david/src/01-fibonacci/__tests__/fibonacci.spec.ts
+++ students/david/src/01-fibonacci/__tests__/fibonacci.spec.ts
@@ -1,10 +1,61 @@
 import assert from 'node:assert';
 import { describe, it } from 'mocha';
 
-describe('Given ...', ()=> {
-  describe('When ...', ()=> {
-    it('Then ...',()=> {
-    assert.equal(true,true);
+import iterativeFibonacci from '../iterativeFibonacci';
+
+describe('Given the function iterativeFibonacci', () => {
+  describe('When is called with a negative number', () => {
+    it('Then it should send `Number must be positive or equal to zero`', () => {
+      let consoleMessage:string | number;
+      const enteredNumber = -3;
+
+      consoleMessage = iterativeFibonacci(enteredNumber);
+
+      assert.equal(consoleMessage,'Number must be positive or equal to zero');
+    });
+  });
+
+  describe('When is called with a float number', () => {
+    it('Then it should send `Number must be an integer`', () => {
+      let consoleMessage:string | number;
+      const enteredNumber = 2.2;
+
+      consoleMessage = iterativeFibonacci(enteredNumber);
+
+      assert.equal(consoleMessage,'Number must be an integer');
+    });
+  });
+
+  describe('When is called with a 0', () => {
+    it('Then it should return 1', () => {
+      let fibonacciNumber:string | number;
+      const enteredNumber = 0;
+
+      fibonacciNumber = iterativeFibonacci(enteredNumber);
+
+      assert.equal(fibonacciNumber,1);
+    });
+  });
+
+  describe('When is called with a 2', () => {
+    it('Then it should return 2', () => {
+      let fibonacciNumber:string | number;
+      const enteredNumber = 2;
+
+      fibonacciNumber = iterativeFibonacci(enteredNumber);
+
+      assert.equal(fibonacciNumber,2);
+    });
+  });
+
+  describe('When is called with a 10', () => {
+    it('Then it should return 89', () => {
+      let fibonacciNumber:string | number;
+      const enteredNumber = 10;
+
+      fibonacciNumber = iterativeFibonacci(enteredNumber);
+
+      assert.equal(fibonacciNumber,89);
     });
   });
 });
