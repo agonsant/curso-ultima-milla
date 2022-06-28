6935ae958276d79761836b69e63b954a5e4af32d
Rodica Fazakas <rodica.fazakas@int.softonic.com>
2022-06-28 18:59:02 +0200

    Added configuration to the progress bar exercise

---
+++ students/rodicaf/ex-3/src/index.ts
@@ -0,0 +1,25 @@
+// Exercise 1
+// The observables are 
+import { fromEvent, of, map, from} from 'rxjs';
+import { concatAll, delay } from 'rxjs/operators';
+
+const items$ = from(['first', 'second', 'third','fourth']);
+
+items$
+  .pipe(
+    map(item => of(item).pipe(delay(1000))),
+    concatAll()
+  )
+  .subscribe( val => console.log(val) );
+
+const progressBar = document.querySelector(".progress-bar");  
+const loadButton = document.querySelector(".load-data")!;
+const data = document.querySelector(".data");
+
+// compute percentage completion
+
+const click$ = fromEvent(loadButton, 'click');
+
+console.log('Hello world');
+
+
---
+++ students/rodicaf/ex-3/webpack.config.js
@@ -0,0 +1,25 @@
+const { join } = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+    entry: '.src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        }
+      ],
+    },
+    output: {
+      filename: 'main.js',
+      path: join(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    // devServer: {
+    //   contentBase: './dist',
+    //   port: 4200,
+    // }
+}
