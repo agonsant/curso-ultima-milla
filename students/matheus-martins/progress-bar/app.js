9ce43a324bd61ca37e47062c23edd9598242c38f
matheus martins <matheus.martins@int.softonic.com>
2022-06-29 14:57:41 +0200

    Refactor progress bar function

--- students/matheus-martins/progress-bar/app.js
+++ students/matheus-martins/progress-bar/app.js
@@ -4,14 +4,12 @@ const { fromEvent, map, interval, take, concatAll } = rxjs;
 const progressBar = document.getElementById('progress-bar-back');
 
 const updateProgress = progressRatio => {
-    console.log("progress:", progressRatio);
-    progressBar.style.width += `${progressRatio}%`;
+    progressBar.style.width = `${progressRatio}%`;
 }
 
 const clicks = fromEvent(document.querySelector('button'), 'click');
 
-const higherOrder = clicks.pipe(
-  map(() => interval(10).pipe(take(100)))
+const loadData = clicks.pipe(
+  map(() => interval(10).pipe(take(101)))
 );
-const firstOrder = higherOrder.pipe(concatAll());
-firstOrder.subscribe(x => updateProgress(x));
+loadData.pipe(concatAll()).subscribe(x => updateProgress(x));
