2218fd1caea9e8bd455d4b9954d15b70431c5614
matheus martins <matheus.martins@int.softonic.com>
2022-06-30 16:21:47 +0200

    Setup battery exercise

--- students/matheus-martins/ex02/progress-bar/app.js
+++ students/matheus-martins/ex02/progress-bar/app.js
@@ -4,12 +4,13 @@ const { fromEvent, map, interval, take, concatAll } = rxjs;
 const progressBar = document.getElementById('progress-bar-back');
 
 const updateProgress = progressRatio => {
+  console.log(progressRatio);
     progressBar.style.width = `${progressRatio}%`;
 }
 
-const clicks = fromEvent(document.querySelector('button'), 'click');
+const clicks$ = fromEvent(document.querySelector('button'), 'click');
 
-const loadData = clicks.pipe(
-  map(() => interval(10).pipe(take(101)))
+const loadData = clicks$.pipe(
+  map(() => interval(10).pipe(take(301)))
 );
 loadData.pipe(concatAll()).subscribe(x => updateProgress(x));
