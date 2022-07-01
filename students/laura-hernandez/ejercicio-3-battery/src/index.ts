05fe0a76713b056dfaf18c4f31ca8246c1590049
Laura Hernandez Varela <lhvarela@gmail.com>
2022-07-01 12:22:28 +0200

    Vanilla JS added to change the styles

--- students/laura-hernandez/ejercicio-3-battery/src/index.ts
+++ students/laura-hernandez/ejercicio-3-battery/src/index.ts
@@ -1,4 +1,11 @@
-import { fromEvent, interval, of } from 'rxjs';
+import { fromEvent, interval, of , from} from 'rxjs';
+
+const cellsIds = from(["cell-1", "cell-2", "cell-3", "cell-4" ]);
+
+cellsIds.subscribe();
+
+const batteryContainer = document.getElementById("battery-container");
+const batteryInfoText = document.getElementById("info-text");
 
 navigator.getBattery().then(battery => {
   function updateAllBatteryInfo(){
@@ -40,5 +47,48 @@ navigator.getBattery().then(battery => {
     console.log("Battery discharging time: "
                  + battery.dischargingTime + " seconds");
   }
+                
+  if (battery.level === 1) {
+    document.getElementById("cell-1").style.background = "#50eb47"
+    document.getElementById("cell-2").style.background = "#50eb47"
+    document.getElementById("cell-3").style.background = "#50eb47"
+    document.getElementById("cell-4").style.background = "#50eb47"
+    batteryContainer.style.background = "#97ec97";
+  }
 
+  if (battery.level < 1) {
+    document.getElementById("cell-1").style.background = "#50eb47"
+    document.getElementById("cell-2").style.background = "#50eb47"
+    document.getElementById("cell-3").style.background = "#50eb47"
+    document.getElementById("cell-4").style.background = "white"
+    batteryContainer.style.background = "#97ec97";
+  }
+
+  if (battery.level < 0.75) {
+    document.getElementById("cell-1").style.background = "#50eb47"
+    document.getElementById("cell-2").style.background = "#50eb47"
+    document.getElementById("cell-3").style.background = "white"
+    document.getElementById("cell-4").style.background = "white"
+    batteryContainer.style.background = "#97ec97";
+  }
+
+  if (battery.level < 0.5) {
+    document.getElementById("cell-1").style.background = "50eb47"
+    document.getElementById("cell-2").style.background = "white"
+    document.getElementById("cell-3").style.background = "white"
+    document.getElementById("cell-4").style.background = "white"
+    batteryContainer.style.background = "#97ec97";
+  } 
+  
+  if (battery.level < 0.25) {
+    document.getElementById("cell-1").style.background = "red"
+    document.getElementById("cell-2").style.background = "white"
+    document.getElementById("cell-3").style.background = "white"
+    document.getElementById("cell-4").style.background = "white"
+    batteryContainer.style.background = "#e89898";
+  } 
+
+  if(battery.charging) {
+    batteryInfoText.innerHTML = "Charging";
+  }
 });
