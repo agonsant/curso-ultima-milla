63912c24b16fc66ba2bf62edb0b58a838f3dca0b
Rodica Fazakas <rodica.fazakas@int.softonic.com>
2022-07-01 17:12:54 +0200

    Battery charger solution

--- students/rodicaf/ex-4/src/index.ts
+++ students/rodicaf/ex-4/src/index.ts
@@ -1,2 +1,47 @@
-// We are going to construct a battery charger
-console.log("Hello world!!!!");
+// Battery charger exercise
+
+import { Observable, from } from 'rxjs';
+
+interface BatteryType {
+  charging: boolean;
+  chargingTime: number;
+  dischargingTime: number;
+  level: number;
+  onchargingchange: number | null;
+  onchargingtimechange: number | null;
+  ondischargingtimechange: number | null;
+  onlevelchange: number | null;
+}
+declare global {
+    interface Navigator {
+    getBattery: () => Promise<BatteryType>;
+    }
+}
+
+const batteryCharger = document.querySelector(".battery-info") as HTMLDivElement;
+const message = document.querySelector(".message") as HTMLDivElement;
+const levelHTML = document.querySelector("level") as HTMLDivElement;
+const getLevel = (data) => {
+  levelHTML.innerHTML = data;
+};
+const getMessage = (data) => {
+  message.innerHTML = data;
+};
+
+(()=>{
+  const batteryStatus$: Observable<BatteryType> = from(navigator.getBattery()); 
+  batteryStatus$.subscribe(
+    battery => { 
+      console.log("Battery status: ", battery.level);
+    //   getLevel(battery.level); 
+      if (battery.level < 0.1) {
+        batteryCharger.classList.add("red");
+        getMessage("Low Battery"); 
+      } else {
+        batteryCharger.classList.add("green");
+        getMessage("Battery Charging");
+      }
+    })  
+})();
+
+
--- students/rodicaf/ex-4/webpack.config.js
+++ students/rodicaf/ex-4/webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     devServer: {
       static: './dist',
     },
+    devtool: "inline-source-map",
     plugins: [
       new HtmlWebpackPlugin({
           template: "src/index.html"
