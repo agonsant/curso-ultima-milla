f838fde901d0a4e2c4086049c9a8e4bafb4547fb
David Galvez <davidgalvezgomez01@gmail.com>
2022-07-04 14:30:33 +0200

    feat: added style to battery

--- students/david/typescript-exercises/src/04-battery/src/index.ts
+++ students/david/typescript-exercises/src/04-battery/src/index.ts
@@ -6,10 +6,26 @@ declare global {
   }
 }
 
+let batteryLevel:number;
+
 const getActualBattery = async()=> {
 const battery:IBatteryType = await navigator.getBattery();
-const batteryLevel = battery.level * 100;
+batteryLevel = battery.level * 100;
+console.log('d',batteryLevel);
 console.log('The currentBattery is:', batteryLevel,'%');
+return batteryLevel;
 };
 
 getActualBattery();
+
+const batteryComponent1 = document.getElementById('battery-indicator1');
+const batteryComponent2 = document.getElementById('battery-indicator2');
+const batteryComponent3 = document.getElementById('battery-indicator3');
+const batteryComponent4 = document.getElementById('battery-indicator4');
+const batteryComponent5 = document.getElementById('battery-indicator5');
+
+console.log('g', batteryLevel)
+if (batteryLevel < 80) {
+  console.log('f');
+  batteryComponent5.style.display = 'none';
+};
