673e93f34017d59d13cc4a42e37146fe78a5e861
Laura Arcidiacono <laura.arcidiacono@int.softonic.com>
2022-07-12 19:58:43 +0200

    feat: add first logic to ArenaPage

--- group-challenges/pokemon-arena-group-01/front-end/src/pages/ArenaPage.tsx
+++ group-challenges/pokemon-arena-group-01/front-end/src/pages/ArenaPage.tsx
@@ -1,7 +1,50 @@
+import React from 'react';
+import { useState, useEffect} from 'react';
+import Loading from '../components/Loading';
+import getOneRandomPokemon from '../helpers/getOneRandomPokemon';
+import { RandomPokemon } from '../models/randomPokemon-model';
+
+
 const ArenaPage = () => {
+
+    const [randomPokemon, setRandomPokemon] = useState<RandomPokemon>();
+
+    const [isLoading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        setLoading(true);
+        getOneRandomPokemon()
+        .then((data) => {
+            setRandomPokemon(data);
+            setLoading(false);
+        });
+    }, []);
+
+    const handleSearchRandomPokemon = () => {
+        setLoading(true);
+        getOneRandomPokemon()
+        .then((data) => {
+            setRandomPokemon(data);
+            setLoading(false);
+        });
+    }
+
+
     return (
         <>
-        <h1>Arena Page</h1>
+        <h2>Arena Page</h2>
+        <button onClick={ handleSearchRandomPokemon }>Search Random Pokemon</button>
+        {isLoading &&
+            <Loading />
+        }
+        <h3>Play with: { randomPokemon?.name.toUpperCase() }</h3>
+        <h3>id: { randomPokemon?.id }</h3>
+        <p>moves name: { randomPokemon?.moves?.map(move => move.move.name) }</p>
+        <p>slot: { randomPokemon?.types?.map(type => type.slot) }</p>
+        <p>stat effort: { randomPokemon?.stats?.map(stat => stat.effort) }</p>
+        {/* <p>sprites: { randomPokemon?.sprites?.map(sprite => sprite.front_default) }</p> */}
+
         </>
     )
 };
