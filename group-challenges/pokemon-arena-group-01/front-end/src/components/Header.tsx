3d8dfea2f43266d3609fb675202a62767db06666
David Galvez <davidgalvezgomez01@gmail.com>
2022-07-12 17:53:28 +0200

    feat: added toggle theme on header button

--- group-challenges/pokemon-arena-group-01/front-end/src/components/Header.tsx
+++ group-challenges/pokemon-arena-group-01/front-end/src/components/Header.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import "./Header.scss";
 export default function Header() {
+  const [isNightModeOn,setIsNightModeOn]=useState(true);
+  const toggleThemeMode = ()=> {
+    setIsNightModeOn(!isNightModeOn);
+  }
   return (
     <div className="header-main-container">
       <h1 className="header-title">Pokemon Arena Battle</h1>
-      <img 
+      <button className="header-theme-button" onClick={toggleThemeMode}>
+      {!isNightModeOn && (
+        <img 
         className="header-theme-toggler"
         src="moon.png" 
         alt="Dark mode icon">
        </img>
-       <img 
+      )}
+      {isNightModeOn && (
+        <img 
         className="header-theme-toggler"
         src="sun.png" 
         alt="Light mode icon">
        </img>
+      )}
+      </button>
+      
     </div>
   );
 }
