111d246acaff9e7abda12746fd0b945be176b2c8
Laura Arcidiacono <laura.arcidiacono@int.softonic.com>
2022-07-12 19:57:56 +0200

    feat: create getOneRandomPokemon function

---
+++ group-challenges/pokemon-arena-group-01/front-end/src/helpers/getOneRandomPokemon.ts
@@ -0,0 +1,23 @@
+const getOneRandomPokemon = async () => {
+    const randomPokemon = Math.floor(Math.random() * (1155 - 1) + 1); // Allow us to select a random number, from 1 to 1154
+                                                                      // (max of Pokemons existents, for now).
+
+    const apiURL = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`; // The random number it's used as id parameter
+
+    const res = await fetch(apiURL);
+    const response = await res.json();
+    const pokemonProperties = {
+        name: response.name,
+        id: response.id,
+        moves: response.moves,
+        types: response.types,
+        stats: response.stats,
+        sprites: response.sprites,
+    }
+    console.log(pokemonProperties);
+    return pokemonProperties;
+};
+
+getOneRandomPokemon();
+
+export default getOneRandomPokemon;
