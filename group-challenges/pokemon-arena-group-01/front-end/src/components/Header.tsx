f14e18fa25a19dd46bf5a27adf010478734e1a38
David Galvez <davidgalvezgomez01@gmail.com>
2022-07-12 14:45:56 +0200

    feat: added scss vars to the project and some style to the header

--- group-challenges/pokemon-arena-group-01/front-end/src/components/Header.tsx
+++ group-challenges/pokemon-arena-group-01/front-end/src/components/Header.tsx
@@ -1,5 +1,18 @@
-export default function Header(){
-    return(
-        <h1>header</h1>
-    )
+import "./Header.scss";
+export default function Header() {
+  return (
+    <div className="header-main-container">
+      <h1 className="header-title">Pokemon Arena Battle</h1>
+      <img 
+        className="header-theme-toggler"
+        src="moon.png" 
+        alt="Dark mode icon">
+       </img>
+       <img 
+        className="header-theme-toggler"
+        src="sun.png" 
+        alt="Light mode icon">
+       </img>
+    </div>
+  );
 }
