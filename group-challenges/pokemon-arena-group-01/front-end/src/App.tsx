d762911e667edbd56fec2c92db09310653051c01
Laura Arcidiacono <laura.arcidiacono@int.softonic.com>
2022-07-13 22:35:05 +0200

    feat: add UserOponent route

--- group-challenges/pokemon-arena-group-01/front-end/src/App.tsx
+++ group-challenges/pokemon-arena-group-01/front-end/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import UserOponent from './pages/UserOponent';
 import ArenaPage from './pages/ArenaPage';
 import RankingPage from './pages/RankingPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -14,12 +15,13 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={ <HomePage/> }/>
+          <Route path="/useroponent" element={ <UserOponent/> }/>
           <Route path="/arena" element={ <ArenaPage /> }/>
           <Route path="/ranking" element={ <RankingPage/> }/>
           <Route path="/*" element={<NotFoundPage/>}/>
         </Routes>
       <Footer />
-    </BrowserRouter> 
+    </BrowserRouter>
   );
 }
 
