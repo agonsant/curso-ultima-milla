f9f4339b4cf759176ae889ca22349654b67d991f
Laura Hernandez Varela <lhvarela@gmail.com>
2022-07-13 12:55:55 +0200

    Basic styles to pokemonCard component

--- group-challenges/pokemon-group-2/client/src/components/PokemonCard.tsx
+++ group-challenges/pokemon-group-2/client/src/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useAPI from "../hooks/useAPI";
+import styled from "styled-components";
 
 const PokemonCard = () => {
   const { getPokemonImage, getPokemonName } = useAPI();
@@ -17,11 +18,18 @@ const PokemonCard = () => {
   
   return (
     <>
-      <p>Some pokemon </p>
-      <p>{pokemonName}</p>
-      <img alt="pokemon fetch" width="100" src={`${urlImage}`}/>
+      <CardContainer>
+        <p>Some pokemon </p>
+        <p>{pokemonName}</p>
+        <img alt="pokemon fetch" width="100" src={`${urlImage}`}/>
+      </CardContainer>
     </>
   );
 };
 
 export default PokemonCard;
+
+const CardContainer = styled.div`
+  border: 1px solid black;
+  border-radius: 10px;
+`
