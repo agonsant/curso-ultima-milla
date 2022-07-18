dd781686b00f3904f6f9fbfe56d7d2cf648394b2
Laura Arcidiacono <laura.arcidiacono@int.softonic.com>
2022-07-18 16:34:29 +0200

    feat: add randomPokemonContextProvider

--- group-challenges/pokemon-arena-group-01/front-end/src/store/context/randomPokemonsContext.tsx
+++ group-challenges/pokemon-arena-group-01/front-end/src/store/context/randomPokemonsContext.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import {  RandomPokemon } from "../../models/randomPokemon-model";
+import { RandomPokemon } from "../../models/randomPokemon-model";
 
 export interface IRandomPokemonsContext {
-    isLoading: boolean;
-    setLoading: (isLoading: boolean) => void;
-    userRandomPokemon: RandomPokemon,
-    setUserRandomPokemon: (pokemon: RandomPokemon) => void;
-    computerRandomPokemon: RandomPokemon,
-    setComputerRandomPokemon: (pokemon: RandomPokemon) => void;
-};
+  isLoading: boolean;
+  setLoading: (isLoading: boolean) => void;
+  userRandomPokemon: RandomPokemon;
+  setUserRandomPokemon: (pokemon: RandomPokemon) => void;
+  computerRandomPokemon: RandomPokemon;
+  setComputerRandomPokemon: (pokemon: RandomPokemon) => void;
+}
 
-export const RandomPokemonsContext = React.createContext<IRandomPokemonsContext>({
+export const RandomPokemonsContext =
+  React.createContext<IRandomPokemonsContext>({
     isLoading: false,
     setLoading: () => {},
     userRandomPokemon: undefined,
     setUserRandomPokemon: () => {},
     computerRandomPokemon: undefined,
     setComputerRandomPokemon: () => {},
-});
+  });
